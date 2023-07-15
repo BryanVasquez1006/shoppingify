8a2d387f4fbb1926c8e44d5b358cbbdf58367c0a
BryanVasquez1006 <bryan.vasqueziraheta@gmail.com>
2023-07-15 00:31:31 -0600

    FIRST SIGHT UI HAS BEEN ALMOST CREATED SUCCESSFULL

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,9 +8,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@100;200;300;400;500;600;700;800;900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400;1,700&family=Nunito+Sans:ital,wght@0,600;1,300&family=Quicksand:wght@300;400;500;600;700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,600;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@100;200;300;400;500;600;700;800;900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400;1,700&family=Nunito+Sans:ital,wght@0,600;1,300&family=Quicksand:wght@300;400;500;600;700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,600;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+        rel="stylesheet">
     <title>Document</title>
 </head>
 
@@ -109,68 +111,164 @@
     {{-- NAV-BAR STARTING HERE --}}
 
 
-    <div class="grid grid-cols-3 grid-rows-3 ">
-       
-        <aside id="cta-button-sidebar" class=" flex-initial w-20 h-screen transition-transform -translate-x-full sm:translate-x-0 col-span-1" aria-label="Sidebar">
-            <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800" >
-                <ul class="space-y-2 h-full font-medium flex flex-col justify-between">
-                    <li class="p-2">
-                        <a href="">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path d="M220-180h150v-250h220v250h150v-390L480-765 220-570v390Zm-60 60v-480l320-240 320 240v480H530v-250H430v250H160Zm320-353Z"/></svg>
+    <div class="flex justify-between">
+
+        <aside id="left" class="sticky bg-white z-50 top-0  left-0 w-20 h-screen" aria-label="Sidebar">
+            <div class="flex flex-col justify-between h-full py-4  bg-white">
+                <div class="w-18 h-24 flex justify-center py-2.5">
+                    <img src="img/logo.svg" alt="logo">
+                </div>
+                <ul class="flex flex-col gap-10 font-normal">
+                    <li class="flex items-center pl-5" onclick="toggleHover(this)">
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960"
+                                width="35">
+                                <path
+                                    d="M377-198v-60h463v60H377Zm0-252v-60h463v60H377Zm0-253v-60h463v60H377ZM189-161q-28.05 0-48.025-19Q121-199 121-227.5t19.5-48q19.5-19.5 48-19.5t47.5 19.975Q255-255.05 255-227q0 27.225-19.387 46.613Q216.225-161 189-161Zm0-252q-28.05 0-48.025-19.681Q121-452.362 121-480t19.975-47.319Q160.95-547 189-547q27.225 0 46.613 19.681Q255-507.638 255-480t-19.387 47.319Q216.225-413 189-413Zm-1-253q-27.637 0-47.319-19.681Q121-705.362 121-733t19.681-47.319Q160.363-800 188-800q27.637 0 47.319 19.681Q255-760.638 255-733t-19.681 47.319Q215.637-666 188-666Z" />
+                            </svg>
                         </a>
                     </li>
-        
-                    <li class="flex flex-col gap-6">
-                        <a href="#" class="flex items-center p-2">
-                            <svg class="items" xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg>
-                        </a>
-        
-                        <a href="#" class="flex items-center p-2">
-                            <svg class="refresh" xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path d="M480-80q-75 0-140.5-28T225-185q-49-49-77-114.5T120-440h60q0 125 87.5 212.5T480-140q125 0 212.5-87.5T780-440q0-125-85-212.5T485-740h-23l73 73-41 42-147-147 147-147 41 41-78 78h23q75 0 140.5 28T735-695q49 49 77 114.5T840-440q0 75-28 140.5T735-185q-49 49-114.5 77T480-80Z"/></svg>
-                        </a>
-        
-                        <a href="#" class="flex items-center p-2">
-                            <svg class="stats" xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path d="M160-160v-440h140v440H160Zm250 0v-640h140v640H410Zm250 0v-280h140v280H660Z"/></svg>
+                    <li class="flex items-center pl-5" onclick="toggleHover(this)">
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960"
+                                width="35">
+                                <path
+                                    d="M480-80q-75 0-140.5-28T225-185q-49-49-77-114.5T120-440h60q0 125 87.5 212.5T480-140q125 0 212.5-87.5T780-440q0-125-85-212.5T485-740h-23l73 73-41 42-147-147 147-147 41 41-78 78h23q75 0 140.5 28T735-695q49 49 77 114.5T840-440q0 75-28 140.5T735-185q-49 49-114.5 77T480-80Z" />
+                            </svg>
                         </a>
                     </li>
-                    <li>
-        
+                    <li class="flex items-center pl-5" onclick="toggleHover(this)">
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960"
+                                width="35">
+                                <path
+                                    d="M284-277h60v-275h-60v275Zm166 0h60v-406h-60v406Zm166 0h60v-148h-60v148ZM180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h600q24 0 42 18t18 42v600q0 24-18 42t-42 18H180Zm0-60h600v-600H180v600Zm0-600v600-600Z" />
+                            </svg>
                         </a>
                     </li>
                 </ul>
+                <div class="flex place-self-center static">
+                    <div class="flex justify-center rounded-full bg-yellow-500 text-center w-10 h-10 pt-2">
+                        <img class="w-" src="img/shopping_cart_FILL0_wght400_GRAD0_opsz48.svg" alt="shopping">
+                    </div>
+                </div>
+
             </div>
         </aside>
 
+        {{-- MIDDLE SECTION STARTING HERE --}}
+        <div class="items flex-grow p-6">
+            <div class="flex flex-row justify-between">
+                <div class="flex w-96 text-2xl">
+                    {{-- <h1 class="me-1 font-bold text-orange-400">Shoppingify</h1> --}}
+                    <h1 class="font-bold">
+                        <span class="text-orange-400">Shoppingify</span>
+                        allows you to take your shopping list wherever you go
+                    </h1>
+                </div>
 
-        <div class="col-span-2">
-            soy columna dos
+                <div class="flex items-center">
+                    <span>
+                        <span class="material-symbols-outlined border-y-2 border-s-2 p-2 bg-white rounded-s-lg">
+                            search
+                        </span>
+                    </span>
+                    <input type="text" name="search" id="" placeholder="search for an item"
+                        class=" border-y-2 border-e-2 p-2 w-50 focus:outline-none bg-white rounded-e-lg ">
+                </div>
+            </div>
+
+            <div>
+                <h2 class="a font-bold text-lg mt-10">Fruits and vegetables</h2>
+                <div class="grid grid-cols-4 gap-3">
+                    <div class=" border- p-3 border rounded-lg drop-shadow-md font-bold flex justify-between ">
+                        <p>Avocado</p>
+                        <button class="material-symbols-outlined hover:text-gray-500">
+                            add
+                        </button>
+                    </div>
+                    <div class=" border- p-3 border rounded-lg drop-shadow-md font-bold flex justify-between ">
+                        <p>Avocado</p>
+                        <button class="material-symbols-outlined hover:text-gray-500">
+                            add
+                        </button>
+                    </div>
+                    <div class=" border- p-3 border rounded-lg drop-shadow-md font-bold flex justify-between ">
+                        <p>Avocado</p>
+                        <button class="material-symbols-outlined hover:text-gray-500">
+                            add
+                        </button>
+                    </div>
+                    <div class=" border- p-3 border rounded-lg drop-shadow-md font-bold flex justify-between ">
+                        <p>Avocado</p>
+                        <button class="material-symbols-outlined hover:text-gray-500">
+                            add
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+
+
         </div>
-    
-        <div class="shopping_list p-8 flex flex-col content-center items-center">
+        {{-- MIDDLE SECTION ENDING HERE --}}
+
+        <div class="shopping_list p-10 flex flex-col content-center ">
             <div class="add_item flex justify-center rounded-3xl">
                 <div class="add_item rounded-3xl text-white w-3/4 h-40 grid grid-cols-2 relative justify-center ">
                     <div class="wine-bottle absolute bottom-10 start-8 col-start-1">
-                        <svg width="81" height="135" viewBox="0 0 81 135" fill="none" xmlns="http://www.w3.org/2000/svg" class="">
-                            <path d="M31.5096 5.27646L16.643 9.25995C16.1203 9.40002 15.6746 9.74201 15.404 10.2107C15.1334 10.6794 15.0601 11.2364 15.2001 11.7591L16.9486 18.2845C17.0887 18.8072 17.4307 19.2529 17.8993 19.5235C18.368 19.7941 18.925 19.8674 19.4477 19.7274L21.9114 19.0673L24.5312 28.8445L34.4706 26.1813L31.8507 16.404L34.3144 15.7439C34.8371 15.6038 35.2828 15.2618 35.5534 14.7931C35.824 14.3245 35.8973 13.7675 35.7572 13.2447L34.0088 6.71934C33.8687 6.1966 33.5267 5.7509 33.058 5.48031C32.5893 5.20971 32.0324 5.13639 31.5096 5.27646Z" fill="#F9A109"/>
-                            <path d="M62.4022 61.0071C49.7276 50.124 40.8227 35.5085 36.9632 19.2546C36.8717 18.8802 36.6535 18.549 36.3456 18.3172C36.0377 18.0854 35.659 17.9674 35.2739 17.9831L34.5184 15.1635L18.8173 19.3706L19.5623 22.151L19.346 22.209C19.1246 22.2686 18.9179 22.3732 18.7388 22.5163C18.5596 22.6594 18.4119 22.8379 18.3049 23.0406C18.1978 23.2433 18.1337 23.466 18.1165 23.6946C18.0994 23.9232 18.1295 24.1529 18.2051 24.3693C24.0622 41.2808 24.0287 57.863 18.1043 74.1161C17.7631 75.0504 17.7341 76.0701 18.0217 77.0221L33.9685 129.669C34.3271 130.842 35.1251 131.832 36.196 132.431C37.2668 133.03 38.5276 133.193 39.7154 132.885L75.2289 123.369C76.4251 123.038 77.4447 122.253 78.0706 121.181C78.6965 120.11 78.8792 118.836 78.5798 117.631L65.4253 65.8918C64.9378 63.994 63.8832 62.29 62.4022 61.0071V61.0071Z" fill="#3F3D56"/>
-                            <path opacity="0.2" d="M28.9801 8.23311C29.2823 9.36092 29.1241 10.5626 28.5403 11.5737C27.9565 12.5849 26.995 13.3227 25.8672 13.6249C24.7394 13.9271 23.5377 13.7689 22.5265 13.1851C21.5154 12.6013 20.7775 11.6398 20.4753 10.512" fill="black"/>
-                            <path d="M61.5071 80.1538L57.2066 81.3061C56.0822 78.752 54.0809 76.6846 51.5646 75.478C49.0482 74.2713 46.1832 74.005 43.4876 74.7273C40.792 75.4496 38.4439 77.1127 36.8681 79.4159C35.2922 81.719 34.5927 84.5101 34.896 87.2843L30.5953 88.4366C30.1377 88.5592 29.712 88.7793 29.3475 89.0818C28.9829 89.3843 28.6881 89.7621 28.4832 90.1892C28.2784 90.6164 28.1683 91.0827 28.1605 91.5564C28.1527 92.03 28.2474 92.4997 28.4382 92.9333L41.2451 122.045L68.0679 114.857L65.6335 83.0765C65.5966 82.5952 65.4541 82.1278 65.2161 81.7078C64.9781 81.2878 64.6505 80.9253 64.2565 80.6463C63.8626 80.3672 63.412 80.1785 62.9367 80.0934C62.4615 80.0082 61.9734 80.0289 61.5071 80.1538Z" fill="#F9A109"/>
-                            </svg>
-                            
+                        <svg width="81" height="135" viewBox="0 0 81 135" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" class="">
+                            <path
+                                d="M31.5096 5.27646L16.643 9.25995C16.1203 9.40002 15.6746 9.74201 15.404 10.2107C15.1334 10.6794 15.0601 11.2364 15.2001 11.7591L16.9486 18.2845C17.0887 18.8072 17.4307 19.2529 17.8993 19.5235C18.368 19.7941 18.925 19.8674 19.4477 19.7274L21.9114 19.0673L24.5312 28.8445L34.4706 26.1813L31.8507 16.404L34.3144 15.7439C34.8371 15.6038 35.2828 15.2618 35.5534 14.7931C35.824 14.3245 35.8973 13.7675 35.7572 13.2447L34.0088 6.71934C33.8687 6.1966 33.5267 5.7509 33.058 5.48031C32.5893 5.20971 32.0324 5.13639 31.5096 5.27646Z"
+                                fill="#F9A109" />
+                            <path
+                                d="M62.4022 61.0071C49.7276 50.124 40.8227 35.5085 36.9632 19.2546C36.8717 18.8802 36.6535 18.549 36.3456 18.3172C36.0377 18.0854 35.659 17.9674 35.2739 17.9831L34.5184 15.1635L18.8173 19.3706L19.5623 22.151L19.346 22.209C19.1246 22.2686 18.9179 22.3732 18.7388 22.5163C18.5596 22.6594 18.4119 22.8379 18.3049 23.0406C18.1978 23.2433 18.1337 23.466 18.1165 23.6946C18.0994 23.9232 18.1295 24.1529 18.2051 24.3693C24.0622 41.2808 24.0287 57.863 18.1043 74.1161C17.7631 75.0504 17.7341 76.0701 18.0217 77.0221L33.9685 129.669C34.3271 130.842 35.1251 131.832 36.196 132.431C37.2668 133.03 38.5276 133.193 39.7154 132.885L75.2289 123.369C76.4251 123.038 77.4447 122.253 78.0706 121.181C78.6965 120.11 78.8792 118.836 78.5798 117.631L65.4253 65.8918C64.9378 63.994 63.8832 62.29 62.4022 61.0071V61.0071Z"
+                                fill="#3F3D56" />
+                            <path opacity="0.2"
+                                d="M28.9801 8.23311C29.2823 9.36092 29.1241 10.5626 28.5403 11.5737C27.9565 12.5849 26.995 13.3227 25.8672 13.6249C24.7394 13.9271 23.5377 13.7689 22.5265 13.1851C21.5154 12.6013 20.7775 11.6398 20.4753 10.512"
+                                fill="black" />
+                            <path
+                                d="M61.5071 80.1538L57.2066 81.3061C56.0822 78.752 54.0809 76.6846 51.5646 75.478C49.0482 74.2713 46.1832 74.005 43.4876 74.7273C40.792 75.4496 38.4439 77.1127 36.8681 79.4159C35.2922 81.719 34.5927 84.5101 34.896 87.2843L30.5953 88.4366C30.1377 88.5592 29.712 88.7793 29.3475 89.0818C28.9829 89.3843 28.6881 89.7621 28.4832 90.1892C28.2784 90.6164 28.1683 91.0827 28.1605 91.5564C28.1527 92.03 28.2474 92.4997 28.4382 92.9333L41.2451 122.045L68.0679 114.857L65.6335 83.0765C65.5966 82.5952 65.4541 82.1278 65.2161 81.7078C64.9781 81.2878 64.6505 80.9253 64.2565 80.6463C63.8626 80.3672 63.412 80.1785 62.9367 80.0934C62.4615 80.0082 61.9734 80.0289 61.5071 80.1538Z"
+                                fill="#F9A109" />
+                        </svg>
+
                     </div>
                     <div class="p-6 col-end-10">
                         <p class="font-bold">Didn't find what you need?</p>
-                        <button class="w-32 p-3 bg-white text-black rounded-lg hover:bg-slate-100 mt-4 font-bold" type="submit">Add item</button>
+                        <button class="w-32 p-3 bg-white text-black rounded-lg hover:bg-slate-100 mt-4 font-bold"
+                            type="submit">Add item</button>
                     </div>
-                    
+
                 </div>
             </div>
+
+            <h1 class=" font-bold text-3xl mt-10">Shopping List</h1>
+            <p class="mt-8 text-zinc-500">Fruits and vegetables</p>
+
+            <div class="item flex justify-between">
+                <p class="flex items-center font-bold">Avocado</p>
+                <span
+                    class=" border-orange-300 text-orange-400 border-2 p-3 rounded-full content-center w-24 text-center">3
+                    pcs</span>
+            </div>
+
         </div>
     </div>
-{{-- NAV-BAR ENDING HERE --}}
+    {{-- NAV-BAR ENDING HERE --}}
 
-  
 
+    <script>
+        function toggleHover(element) {
+            const listItems = document.querySelectorAll('.flex.items-center');
+
+            listItems.forEach(item => {
+                item.classList.remove('border-l-4', 'border-yellow-500');
+            });
+
+            element.classList.add('border-l-4', 'border-yellow-500');
+        }
+    </script>
 
 </body>
 
